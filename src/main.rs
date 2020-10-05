a78e21f25fa97eb828004325e71536d63784edce
essenius <essenius@users.noreply.github.com>
2020-10-05 08:34:28 +0200

    Refactored ImageData to have methods

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,6 @@ use filetime::FileTime;
 use walkdir::{DirEntry, WalkDir};
 use std::io::ErrorKind;
 
-// next step: more structure
-
 static DUPLICATE_EXTENSION: &str = "duplicate";
 
 #[derive(Clone)]
@@ -18,15 +16,53 @@ struct ImageData {
     create_time: FileTime,
     size: u64,
     is_duplicate: bool,
+    hash: Option<String>,
 }
 
-fn hash(file: &String) -> Result<String, io::Error> {
-    let path = Path::new(file);
+impl ImageData {
+    fn new(path: &Path) -> ImageData {
+        let metadata = fs::metadata(&path).unwrap();
+        let create_time = get_create_time(&metadata);
+        correct_zero_modification_date(&path, &metadata, &create_time);
+        let is_duplicate = is_duplicate(&path);
+        let name = format!("{}", path.display());
+        ImageData { path: name.clone(), size: metadata.len(), create_time: create_time, is_duplicate: is_duplicate, hash: None}
+    }
 
-    let mut file = File::open(&path)?;
-    let mut sha256 = Sha256::new();
-    io::copy(&mut file, &mut sha256).expect("copy failed");
-    Ok(format!("{:x}", sha256.finalize()))
+    fn is_duplicate(&self) -> bool {
+        let mut is_duplicate = false;
+        if let Some(extension) = Path::new(&self.path).extension() {
+            is_duplicate = extension == DUPLICATE_EXTENSION;
+        }
+        return is_duplicate;
+    }
+
+    fn mark_duplicate(&mut self) {
+        self.is_duplicate = true;
+        let new_duplicate_name = format!("{}.{}", &self.path, DUPLICATE_EXTENSION);
+        println!("Renaming {} to {}", &self.path, &new_duplicate_name);
+        fs::rename(&self.path, &new_duplicate_name).unwrap();
+        self.path = new_duplicate_name;
+    }
+
+    /* fn full_name(&self) {
+        format!("{}", Path::new(&self.path).display())
+    } */
+
+    fn hash(&mut self) -> Result<String, io::Error> {
+        match &self.hash {
+            None => {
+                println!("Calculating hash for {}", &self.path);
+                let path = Path::new(&self.path);    
+                let mut file = File::open(&path)?;
+                let mut sha256 = Sha256::new();
+                io::copy(&mut file, &mut sha256).expect("copy failed");
+                self.hash = Some(format!("{:x}",sha256.finalize()));
+                Ok(self.hash.clone().unwrap())
+            },
+            Some(hash) => Ok(hash.clone()),
+        }
+    }
 }
 
 fn add_to_logfile(original: &String, duplicate: &String) {
@@ -103,21 +139,17 @@ fn images_in(folder: &Path) -> Vec<ImageData> {
             } 
             continue;
         }
-        let path = entry.path();
-        let metadata = fs::metadata(&path).unwrap();
-
-        let create_time = get_create_time(&metadata);
-        correct_zero_modification_date(&path, &metadata, &create_time);
-        let is_duplicate = is_duplicate(&path);
-        if is_duplicate {
-            duplicate_count +=1;
+        let image = ImageData::new(&entry.path());
+        if image.is_duplicate() {
+            duplicate_count += 1;
+            print!("#");
+        } else {
+            print!(".");
+            images.push(image);
         }
-        let name = format!("{}",path.display());
-        images.push(ImageData{path: name.clone(), size: metadata.len(), create_time: create_time, is_duplicate: is_duplicate});
-        print!("{}", if is_duplicate {"#"} else {"."});
         io::stdout().flush().unwrap();
     }
-    println!("Found {} files, {} existing duplicates.", &images.len(), duplicate_count);
+    println!(" Found {} files, excluding {} existing duplicates.", &images.len(), duplicate_count);
     return images;
 }
 
@@ -125,37 +157,27 @@ fn mark_duplicates(images: &mut Vec<ImageData>) {
     let mut duplicate_count = 0;
     let mut duplicate_size = 0;
     let mut previous_percentage = 101;
-    for i in 0..images.len() {
-        let base_entry = images[i].clone();
-        if base_entry.is_duplicate {
+    for base_entry in 0..images.len() {
+        if images[base_entry].is_duplicate {
             continue;
         }
         // Per 5 percent (* 20 = * 100 /5)
-        let percentage = (i * 20 / images.len()) * 5;
+        let percentage = (base_entry * 20 / images.len()) * 5;
         if previous_percentage != percentage {
             println!("{}", format!("{}%", percentage));
             previous_percentage = percentage;
         }
-        let mut j = i+1; 
-        let mut base_hash_calculated = false;
-        let mut base_hash = String::from("");
-        while j < images.len() && images[j].size == base_entry.size {
-            if !images[j].is_duplicate { 
-                if !base_hash_calculated {
-                    base_hash = hash(&base_entry.path).unwrap();
-                    base_hash_calculated = true;
-                }
-                let potential_dup_hash = hash(&images[j].path).unwrap();
-                if potential_dup_hash.eq(&base_hash) {
-                    images[j].is_duplicate = true;
+        let mut candidate_dup = base_entry + 1; 
+        while candidate_dup < images.len() && images[candidate_dup].size == images[base_entry].size {
+            if !images[candidate_dup].is_duplicate { 
+                if images[candidate_dup].hash().unwrap().eq(&images[base_entry].hash().unwrap()) {
+                    images[candidate_dup].mark_duplicate();
                     duplicate_count += 1;
-                    duplicate_size += images[j].size;                
-                    let new_duplicate_name = format!("{}.{}", &images[j].path, DUPLICATE_EXTENSION);
-                    fs::rename(&images[j].path, &new_duplicate_name).unwrap(); 
-                    add_to_logfile(&base_entry.path, &new_duplicate_name);
+                    duplicate_size += images[candidate_dup].size;                
+                    add_to_logfile(&images[base_entry].path, &images[candidate_dup].path);
                 }
             }
-            j+=1;
+            candidate_dup += 1;
         }
     }
     println!("New duplicates found: {}, total size: {}", duplicate_count, duplicate_size);
